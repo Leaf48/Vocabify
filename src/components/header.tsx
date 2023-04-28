41921cd42a414b17c3d2298a132873b999486e8a
leaf48 <watavita2004@gmail.com>
2023-04-27 23:27:39 -0400

    updated

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <div className='text-9xl font-mono  text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-400 '>Header</div>
+      <button className="btn btn-dark" type="button">Dark</button>
+    </>
+    
+  )
+}
+
+export default Header
