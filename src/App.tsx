3e22a3293ac709c304115b1152cd6f848a18e2eb
leaf48 <watavita2004@gmail.com>
2023-04-27 23:53:09 -0400

    setup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Header from './components/header'
+import Home from './components/pages/home'
+import About from './components/pages/about'
+import {Route, Routes} from "react-router-dom"
+
 
 function App() {
   return (
-    <div>
-        <Header></Header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+    </>
   )
 }
 
