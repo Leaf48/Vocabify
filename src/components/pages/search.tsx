e6345f20f89da625948e042e314720d6b33e7a9c
leaf48 <watavita2004@gmail.com>
2023-05-03 00:05:08 -0400

    updated

--- src/components/pages/search.tsx
+++ src/components/pages/search.tsx
@@ -1,49 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import * as cheerio from "cheerio"
 
 const api = {
-  noun: 'http://localhost:5000/scrape?url=https://www.wordhippo.com/what-is/the-noun-for/',
-};
-
-const scrape = async (url: string, word: string): Promise<Array<string>> => {
-  try {
-    const fullURL = url + word + '.html';
-    const { data } = await axios.get(fullURL);
-    const $ = cheerio.load(data);
-
-    const words: Array<string> = []
-    $('div table tbody tr .defv2wordtype').each((nm, elm) => {
-      const w = $(elm).text()
-      words.push(w)
-    })
-
-    console.log(words)
-    return words;
-  } catch (e) {
-    console.error(e);
-    return [];
-  }
-};
-
-function Search() {
-  const [noun, setNoun] = useState<string>('');
-
-  console.log('hello');
-
-  useEffect(() => {
-    console.log('USEEFFECT');
-    const res = async () => {
-      const r = await scrape(api['noun'], 'philanthropist');
-      setNoun(r[0]);
-    };
-    res();
-  }, []);
-
-  return (
-    // <div>{ip}</div>
-    <div>{noun}</div>
-  );
+  "noun": "http://localhost:5000/scrape?url=https://www.wordhippo.com/what-is/the-noun-for/"
 }
 
-export default Search;
+const scrape = async (url: string, word: string): Promise<Array<string>> => {
+  try{
+    const fullURL = url + word + ".html"
+    const {data} = await axios.get(fullURL)
+    const $ = cheerio.load(data)
+
+    const words: Array<string> = []
+
+    $("div table tbody tr .defv2wordtype").each((index, element) => {
+      words.push($(element).text())
+    })
+    
+    return words
+  }catch(e){
+    console.error(e)
+    return []
+  }
+}
+
+const getIP = async (): Promise<string> => {
+  const r = await axios.get("https://api.ipify.org/?format=plane")
+  return r["data"]
+}
+
+function Search() {
+  const [ip, setIP] = useState<string>("")
+  const [noun, setNoun] = useState<string>("")
+
+  // word
+  useEffect(() => {
+    console.log("WORD")
+    const res = async () => {
+      const r = await scrape(api["noun"], "philanthropist")
+      console.log(r)
+      setNoun(r[0])
+    }
+    res()
+  }, [])
+
+  // ip
+  useEffect(() => {
+    console.log("IP")
+    getIP().then(r => {
+      setIP(r)
+    })
+  }, [])
+
+  return (
+    <>
+      <div>{ip}</div>
+      <div>{noun}</div>
+    </>
+  )
+}
+
+export default Search
