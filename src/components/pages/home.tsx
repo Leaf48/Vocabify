03ed82d844fc2808c34f84942241694f24faa210
leaf48 <watavita2004@gmail.com>
2023-07-01 15:40:44 -0400

    Update home.tsx

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>This is a home</div>
+    <div className='text-white font-mono'>
+      <div>This is a home</div>
+      <Link to="/search">SEARCH</Link>
+    </div>
   )
 }
 
 export default Home
+
+// Xボタン押してテキストボックスへフォーカス
+// 単語を押すと、上にスクロール
+// ページの下にバツボタン配置して、テキストボックス削除＋上にスクロール
+// AI on/off & AI 残りカウント
+// 単語の横にコピーアイコン表示Click to Copy
